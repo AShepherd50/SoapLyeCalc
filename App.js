98317eb38d72529d6b751e2a3adcd0f831640231
AShepherd50 <redRHINO321!>
2020-04-21 16:36:46 -0400

    Fats List

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Soap Lye Calculator</Text>
+      <View style={styles.inputRow}>
+        <Text></Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4A646C',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  inputRow:{
+    flexDirection: "row"
+  },
+
+  title:{
+    fontSize: 26,
+    color: 'white',
+  },
+
+  inputTitle:{
+    color: "white",
+    fontSize: 14
+  },
+
 });
---
+++ Fats.js
@@ -0,0 +1,286 @@
+
+const Fats = [{name:"Almond Oil",
+               lye: 0.1367,
+               potash: 0.1925},
+
+              {name: "Aloe Vera Butter",
+               lye: 0.1788,
+               potash: 0.2518},
+
+              { name: "Aloe Vera Oil",
+                lye: 0.1421,
+                potash: 0.2001},
+
+              { name: "Apricot Kernel Oil",
+                lye: 0.1378,
+                potash: 0.1941},
+
+              { name: "Avocado Butter",
+                lye: 0.1339,
+                potash: 0.1886},
+
+              { name: "Avocado Oil",
+                lye: 0.1337,
+                potash: 0.1883},
+
+              { name: "Babassu Nut Oil",
+                lye: 0.1749,
+                potash: 0.2463},
+
+              { name: "Beeswax",
+                lye: 0.0689,
+                potash: 0.0970},
+
+              { name: "Borage Oil",
+                lye: 0.1339,
+                potash: 0.1886},
+
+              { name: "Candelilla Wax",
+                lye: 0.0322,
+                potash: 0.0454},
+
+              { name: "Canola Oil",
+                lye: 0.1328,
+                potash: 0.1870},
+
+              { name: "Canola Oil, High Oleic Acid",
+                lye: 0.1330,
+                potash: 0.1873},
+
+              { name: "Castor Bean Oil",
+                lye: 0.1286,
+                potash: 0.1811},
+
+              { name: "Cherry Kernel Oil",
+                lye: 0.1389,
+                potash: 0.1956},
+
+              { name: "Chicken Fat",
+                lye: 0.1356,
+                potash: 0.1910},
+
+              { name: "Cocoa Butter",
+                lye: 0.1378,
+                potash: 0.1941},
+
+              { name: "Coconut Oil, Refined 76",
+                lye: 0.1910,
+                potash: 0.2690},
+
+              { name: "Coconut Oil, Hydrogenated 92",
+                lye: 0.1910,
+                potash: 0.2690},
+
+              { name: "Coconut Oil, Fractionated/Saturated",
+                lye: 0.2321,
+                potash: 0.3269},
+
+              { name: "Copha Vegetable Shortening",
+                lye: 0.1910,
+                potash: 0.2690},
+
+              { name: "Corn Oil",
+                lye: 0.1368,
+                potash: 0.1927},
+
+              { name: "Cottonseed Oil",
+                lye: 0.1387,
+                potash: 0.1954},
+
+              { name: "Crisco Vegetable Shortening",
+                lye: 0.1369,
+                potash: 0.1928},
+
+              { name: "Emu Oil",
+                lye: 0.1377,
+                potash: 0.1939},
+
+              { name: "Evening Primrose Oil",
+                lye: 0.1362,
+                potash: 0.1918},
+
+              { name: "Flaxseed Oil",
+                lye: 0.1358,
+                potash: 0.1913},
+
+              { name: "Goat Fat",
+                lye: 0.1382,
+                potash: 0.1946},
+
+              { name: "Goose Fat",
+                lye: 0.1349,
+                potash: 0.1900},
+
+              { name: "Grapeseed Oil",
+                lye: 0.1321,
+                potash: 0.1861},
+
+              { name: "Hazelnut Oil",
+                lye: 0.1369,
+                potash: 0.1928},
+
+              { name: "Hempseed Oil",
+                lye: 0.1359,
+                potash: 0.1914},
+
+              { name: "Jojoba Seed Oil",
+                lye: 0.0695,
+                potash: 0.0979},
+
+              { name:"Jojoba Seed Liquid Wax",
+                lye: 0.0695,
+                potash: 0.0979},
+
+              { name: "Karite Butter",
+                lye: 0.1296,
+                potash: 0.1825},
+
+              { name: "Kremelta Vegetable Shortening",
+                lye: 0.1910,
+                potash: 0.2690},
+
+              { name: "Kukui Nut Oil",
+                lye: 0.1351,
+                potash: 0.1903},
+
+              { name: "Lanolin",
+                lye: 0.0748,
+                potash: 0.1054},
+
+              { name: "Lard",
+                lye: 0.1399,
+                potash: 0.1970},
+
+              { name: "Linseed Oil",
+                lye: 0.1358,
+                potash: 0.1913},
+
+              { name: "Macadamia Nut Oil",
+                lye: 0.1391,
+                potash: 0.1959},
+
+              { name: "Milk Fat",
+                lye: 0.1599,
+                potash: 0.2252},
+
+              {name: "Mink Oil",
+               lye: 0.1403,
+               potash: 0.1976},
+
+              { name: "Monoi de Tahiti Oil",
+                lye: 0.1796,
+                potash: 0.2530},
+
+              { name: "Neem Tree Oil",
+                lye: 0.1372,
+                potash: 0.1932},
+
+              { name:"Olive Oil",
+                lye: 0.1353,
+                potash: 0.1906},
+
+              { name: "Ostrich Oil",
+                lye: 0.1385,
+                potash: 0.1951},
+
+              { name: "Palm Kernel Oil",
+                lye: 0.1777,
+                potash: 0.2503},
+
+              { name: "Palm Oil",
+                lye: 0.1420,
+                potash: 0.2000},
+
+              { name: "Peach Kernel Oil",
+                lye: 0.1361,
+                potash: 0.1917},
+
+              { name: "Peanut Oil",
+                lye: 0.1367,
+                potash: 0.1925},
+
+              { name: "Pine Rosin",
+                lye: 0.1298,
+                potash: 0.1820},
+
+              { name: "Pumpkin Seed Oil",
+                lye: 0.1389,
+                potash: 0.1956},
+
+              { name: "Rapeseed Oil",
+                lye: 0.1328,
+                potash: 0.1870},
+
+              { name: "Rice Bran Oil",
+                lye: 0.1284,
+                potash: 0.1808},
+
+              { name: "Safflower Oil, High Linoleic Acid",
+                lye: 0.1374,
+                potash: 0.1935},
+
+              { name: "Safflower Oil, High Oleic Acid",
+                lye: 0.1369,
+                potash: 0.1928},
+
+              { name: "Sesame Seed Oil",
+                lye: 0.1336,
+                potash: 0.1882},
+
+              { name: "Shea Butter",
+                lye: 0.1296,
+                potash: 0.1825},
+
+              { name: "Soybean Oil",
+                lye: 0.1359,
+                potash: 0.1914},
+
+              { name: "Soybean Oil, 27.5% Hydrogenated",
+                lye: 0.1361,
+                potash: 0.1917},
+
+              { name: "Stearic Acid, Animal-Source",
+                lye: 0.1413,
+                potash: 0.1990},
+
+              { name: "Stearic-Acid, Vegetable-Source",
+                lye: 0.1411,
+                potash: 0.1987},
+
+              { name: "Sunflower Seed Oil",
+                lye: 0.1358,
+                potash: 0.1913},
+
+              { name: "Sunflower Seed Oil, High Oleic Acid",
+                lye: 0.1351,
+                potash: 0.1903},
+
+              { name: "Tallow, Beef",
+                lye: 0.1419,
+                potash: 0.1999},
+
+              { name: "Tallow, Deer",
+                lye: 0.1382,
+                potash: 0.1946},
+
+              { name: "Tallow, Sheep",
+                lye: 0.1384,
+                potash: 0.1949},
+
+              { name: "Tamanu Seed Oil",
+                lye: 0.1437,
+                potash: 0.2024},
+
+              { name: "Tiare Flower Oil",
+                lye: 0.1796,
+                potash: 0.2530},
+
+              { name: "Walnut Oil",
+                lye: 0.1349,
+                potash: 0.1900},
+
+              { name: "Wheat Germ Oil",
+                lye: 0.1319,
+                potash: 0.1858}
+            ]
+
